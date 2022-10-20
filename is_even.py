9fbf6ef551ad890439cbed3c5d658f1145b81602
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-20 21:30:56 +0500

    is_even

--- is_even.py
+++ is_even.py
@@ -1,3 +1,10 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is even, otherwise print 0.
+#from  import new_module
+
+
+number = 16
+a = number%2
+print(a+1)
+
