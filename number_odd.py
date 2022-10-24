66c5f500839d931ee7b9b1a7092dba764663990d
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-24 17:40:53 +0500

    number_odd01

--- number_odd.py
+++ number_odd.py
@@ -1,5 +1,15 @@
-#A four-digit integer is given. Find the number of odd digits in it.
+#A four-digit integer is given. Find the number of even digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
 
-#Print the number of odd digits in the variable "var_int".
+#Print the number of even digits in the variable "var_int".
+var_int = 4698
+a = var_int%10
+b = var_int%100//10
+c = var_int//100%10
+d = var_int//1000
+a_1 = (a+1)%2
+b_1 = (b+1)%2
+c_1 = (c+1)%2
+d_1 = (d+1)%2
+print(a_1+b_1+c_1+d_1)
